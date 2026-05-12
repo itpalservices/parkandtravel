dcd8f3969cb861e04f8380f5dee38aa6e009969d
Nicolas <nicolas.charalambus@gmail.com>
2026-05-12 19:12:54 +0300

    Footer added in the whole system

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeScreenPromptComponent } from './components/home-screen-prompt/home-screen-prompt.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeScreenPromptComponent],
+  imports: [RouterOutlet, HomeScreenPromptComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
