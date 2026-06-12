f0d43ad98c1b50af3fde045fcee2b56d6e1886be
Nicolas <nicolas.charalambus@gmail.com>
2026-06-12 22:24:46 +0300

    Attempt 3

--- frontend/src/app/core/services/zebra-print.service.ts
+++ frontend/src/app/core/services/zebra-print.service.ts
@@ -24,6 +24,11 @@ export class ZebraPrintService {
       throw new Error('No Zebra printer found. Make sure the ZQ521 is connected in Browser Print.');
     }
 
+    // Ensure printer is in ZPL mode (works even if printer is currently in CPCL mode)
+    await firstValueFrom(
+      this.http.post(`${this.agentUrl}/write`, { device: printer, data: '! U1 setvar "device.languages" "ZPL"\r\n' }, { responseType: 'text' })
+    ).catch(() => {});
+
     await firstValueFrom(
       this.http.post(`${this.agentUrl}/write`, { device: printer, data: zpl }, { responseType: 'text' })
     );
